1adb5bef1be9b45b6e356f9d0c5e2fa5022a4d9e
yubowennjust <yubowen_njust@163.com>
2018-08-28 13:21:29 +0800

    1

--- 1.tensorflow/TensorFlow实战Google深度学习框架/unit3/1.py
+++ 1.tensorflow/TensorFlow实战Google深度学习框架/unit3/1.py
@@ -25,3 +25,24 @@ result=a+b
 
 with tf.Session() as sess:
     print(sess.run(result))
+
+
+
+w1=tf.Variable(tf.random_normal([2,3],stddev=1))
+w2=tf.Variable(tf.random_normal([3,1],stddev=1))
+
+# x=tf.constant([[0.7,0.9]])
+x=tf.placeholder(tf.float32,shape=(3,2),name="input")
+
+a=tf.matmul(x,w1)
+y=tf.matmul(a,w2)
+
+with tf.Session() as sess1:
+    # sess1.run(w1.initializer)
+    # sess1.run(w2.initializer)
+    # print(sess1.run(y))
+    init_op = tf.global_variables_initializer()
+    sess1.run(init_op)
+    print(sess1.run(y,feed_dict={x:[[0.7,0.9],[0.4,0.6],[0.1,0.4]]}))
+
+
