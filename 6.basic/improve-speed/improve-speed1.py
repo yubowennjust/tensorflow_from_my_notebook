f41540204b6b3d9cd82830ded24edeaa3969cc9c
yubowennjust <yubowen_njust@163.com>
2018-11-02 00:33:59 +0800

    1

---
+++ 6.basic/improve-speed/improve-speed1.py
@@ -0,0 +1,40 @@
+import glob
+import concurrent.futures
+import os
+import cv2
+import time
+import datetime
+
+
+# count = 0
+# start_time = time.time() # 记录时间
+# for filename in os.listdir(r"D:\\Pictures\\test"):              #listdir的参数是文件夹的路径
+#     print("D:\\Pictures\\test\\"+filename)
+#     count += 1
+#     print(count)
+#     img = cv2.imread("D:\\Pictures\\test\\"+filename)
+#     img = cv2.resize(img,(600,600))
+# duration = time.time() - start_time #
+# print(duration)
+
+def load_and_resize(image_filename):
+    ### Read in the image data
+    img = cv2.imread(image_filename)
+
+    ### Resize the image
+    img = cv2.resize(img, (600, 600))
+
+
+image_files = glob.glob('D:\\Pictures\\test\\*.*')
+print(len(image_files))
+print(image_files[1])
+
+for i in range(len(image_files)):
+    print(image_files[i])
+    img = cv2.imread(image_files[i])
+    img = cv2.resize(img, (600, 600))
+
+
+
+
+
